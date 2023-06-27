1896ff1da137d10fbd446f3d9a492d7cb786243e
ironshadowz <chenwl911@gmail.com>
2023-06-27 09:33:08 +0800

    initial setup

---
+++ src/main/java/sg/edu/nus/iss/tut24/Tut24Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.tut24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tut24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tut24Application.class, args);
+	}
+
+}
